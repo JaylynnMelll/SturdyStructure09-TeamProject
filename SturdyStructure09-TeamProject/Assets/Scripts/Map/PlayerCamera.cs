51fad21d0b585e7da1291059f4ec1e168cef102c
yoonjin1127 <129118497+yoonjin1127@users.noreply.github.com>
2025-05-12 09:45:54 +0900

    feat: 보스맵 타일 추가
    보스맵 타일 추가

---
+++ SturdyStructure09-TeamProject/Assets/Scripts/Map/PlayerCamera.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+
+    public float cameraSpeed = 5.0f;
+
+    // 플레이어 이동은 Update이므로 겹치지 않게 뒤에 처리
+    private void LateUpdate()
+    {
+        // z축은 그대로 유지
+        Vector3 targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
+
+        // 보간
+        transform.position = Vector3.Lerp(transform.position, targetPosition, cameraSpeed * Time.deltaTime);
+    }
+}
